62f4d02670cc4bc526c2e1de4e05924ac81db5a4
Alexis Cervantes <iacervantes@outlook.com>
2024-10-16 12:39:38 -0300

    Atualizando...

--- 0_fundamentos/desafios-projetos_0/System-Banking.py
+++ 0_fundamentos/desafios-projetos_0/System-Banking.py
@@ -1,5 +1,8 @@
-menu = """Seja Bem-vindo(a) - Para continuar escolha uma opção:
-[C] criar conta    [D] depositar    [S] sacar    [E] extrato    [Q] sair : """
+external_menu = """Seja Bem-vindo(a) - Escolha uma Opção:
+[C] criar conta    [Q] sair : """
+internal_menu = """Seja Bem-vindo(a) - Escolha uma Opção:
+[D] depositar    [S] sacar    [E] extrato    [Q] sair : """
+
 
 saldo = 0
 extrato = ""
@@ -10,20 +13,19 @@ user_name = ''
 account_number = 260674
 
 while True:
-    opcao = input(menu)
+    opcao = input(internal_menu)
 
     if opcao == "C".lower():
         print('\n'+12*'#'+' CRIAR CONTA '+'#'*12)
         user_name = input("Informe seu nome completo: ")
-        # try:
         if all(c.isalpha() or c.isspace() for c in user_name):
             print(f'Seja Bem-vindo {user_name}')
             print(f'Número de conta: {account_number+1}\n')
         else: print('\033[0;31mDigite valores alfabeticos. Tente de novo\033[m')
-        # except ValueError: print(f'Valor inválido. Digite valores numéricos e inteiros\n')
+
     elif opcao == "D".lower():
         print('\n'+12*'#'+' DEPÓSITO '+'#'*12)
-
+        print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
         try:
             valor = float(input("Informe o valor do Depósito: "))
             if valor > 0:
@@ -31,7 +33,6 @@ while True:
                 extrato += f"Depósito: R$ {valor:.2f}\n"
                 print(f'Foram depositados: R$ {valor}\n')
             elif user_name == "": print('\033[0;31mCrie uma conta primeiro\033[m')
-
             else: print("\033[0;31mValor fornecido ivalido. Informe valores positivos e maiores de '0'\033[m\n")
         except ValueError: print(f'\033[0;31mValor inválido. Digite valores numéricos e inteiros\033[m\n')
 
@@ -41,16 +42,13 @@ while True:
         try:
             print(f'{numero_saques+1} SAQUE')
             valor = float(input("Informe o valor do Saque: "))
-            print(f'Foram retirados: R$ {valor}\n')
+            print(f'Foram retirados: R$ {valor}')
             excedeu_saldo = valor > saldo
             excedeu_limite = valor > limite
             excedeu_saques = numero_saques >= LIMITE_SAQUES
-
             if excedeu_saldo: print("\n\033[0;31mNão tem saldo suficiente\033[m\n")
             elif excedeu_limite: print("\033[0;31mValor excede o limite de R$ 500.00 por SAQUE\033[m\n")
-
             elif excedeu_saques: print("\n\033[0;31mVocê atingiu o número máximo de saques\033[m\n")
-
             elif valor > 0:
                 saldo -= valor
                 extrato += f"Saque: R$ {valor:.2f}\n"
@@ -59,12 +57,13 @@ while True:
         except ValueError: print("\n\033[0;31mValor informado é inválido. Digite valores numéricos e inteiros.\033[m\n")
 
     elif opcao == "E".lower():
-        print('\n'+20*'#'+' EXTRATO '+'#'*20)
+        print('\n'+25*'#'+' EXTRATO '+'#'*25)
         print(f'Usuario: {user_name} - número de Conta: {account_number} ')
-        print(40*'-')
+        print(60*'-')
         print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(60*'-')
         print(f"Saldo: R$ {saldo:.2f}")
-        print(40*'#'+'\n')
+        print(60*'#'+'\n')
 
     elif opcao == "Q".lower():
         print(14*'#'+' SAINDO DO SISTEMA '+'#'*14)
---
+++ 0_fundamentos/estrutura_repeticao_while/while_aninhada.py
@@ -0,0 +1,24 @@
+num_alunos = 3
+num_notas = 3
+
+# Contador para o número de alunos
+aluno = 0
+
+while aluno < num_alunos:
+    print(f"Anotando as notas do aluno {aluno + 1}")
+    # Contador para o número de notas de cada aluno
+    nota = 0
+    total_notas = 0
+
+    while nota < num_notas:
+        nota_atual = float(input(f"Digite a nota {nota + 1} do aluno {aluno + 1}: "))
+        total_notas += nota_atual
+        nota += 1
+
+    media = total_notas / num_notas
+    print(f"A média do aluno {aluno + 1} é: {media:.2f}\n")
+
+    aluno += 1
+
+# Mensagem final após todas as médias serem recebidas
+print("Todas as médias foram recebidas!")
