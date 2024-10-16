c2ad50a887d74f0c703dfa46ce8ef8c5162078f2
Alexis Cervantes <iacervantes@outlook.com>
2024-10-16 07:27:30 -0300

    Atualizando sistema bancario

--- 0_fundamentos/desafios-projetos_0/System-Banking.py
+++ 0_fundamentos/desafios-projetos_0/System-Banking.py
@@ -1,28 +1,43 @@
 menu = """Seja Bem-vindo(a) - Para continuar escolha uma opção:
-[D] depositar    [S] sacar    [E] extrato    [Q] sair : """
-criar_conta = """Vamos criar sua nova conta"""
+[C] criar conta    [D] depositar    [S] sacar    [E] extrato    [Q] sair : """
 
 saldo = 0
 extrato = ""
 limite = 500
 numero_saques = 0
 LIMITE_SAQUES = 3
+user_name = ''
+account_number = 260674
 
 while True:
     opcao = input(menu)
-    if opcao == "D".lower():
+
+    if opcao == "C".lower():
+        print('\n'+12*'#'+' CRIAR CONTA '+'#'*12)
+        user_name = input("Informe seu nome completo: ")
+        # try:
+        if all(c.isalpha() or c.isspace() for c in user_name):
+            print(f'Seja Bem-vindo {user_name}')
+            print(f'Número de conta: {account_number+1}\n')
+        else: print('\033[0;31mDigite valores alfabeticos. Tente de novo\033[m')
+        # except ValueError: print(f'Valor inválido. Digite valores numéricos e inteiros\n')
+    elif opcao == "D".lower():
         print('\n'+12*'#'+' DEPÓSITO '+'#'*12)
+
         try:
             valor = float(input("Informe o valor do Depósito: "))
             if valor > 0:
                 saldo += valor
                 extrato += f"Depósito: R$ {valor:.2f}\n"
                 print(f'Foram depositados: R$ {valor}\n')
-            else: print("Deposito - Valor ivalido. Informa valores positivos maiores de '0'\n")
-        except ValueError: print(f'Valor inválido. Digite valores numéricos e inteiros\n')
+            elif user_name == "": print('\033[0;31mCrie uma conta primeiro\033[m')
+
+            else: print("\033[0;31mValor fornecido ivalido. Informe valores positivos e maiores de '0'\033[m\n")
+        except ValueError: print(f'\033[0;31mValor inválido. Digite valores numéricos e inteiros\033[m\n')
 
     elif opcao == "S".lower():
         print('\n'+12*'#'+' SAQUE '+'#'*12)
+        print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
         try:
             print(f'{numero_saques+1} SAQUE')
             valor = float(input("Informe o valor do Saque: "))
@@ -31,27 +46,30 @@ while True:
             excedeu_limite = valor > limite
             excedeu_saques = numero_saques >= LIMITE_SAQUES
 
-            if excedeu_saldo: print("\nNão tem saldo suficiente")
-            elif excedeu_limite: print("Valor excede o limite de R$ 500.00 por SAQUE\n")
+            if excedeu_saldo: print("\n\033[0;31mNão tem saldo suficiente\033[m\n")
+            elif excedeu_limite: print("\033[0;31mValor excede o limite de R$ 500.00 por SAQUE\033[m\n")
 
-            elif excedeu_saques: print("\nVocê atingiu o número máximo de saques")
+            elif excedeu_saques: print("\n\033[0;31mVocê atingiu o número máximo de saques\033[m\n")
 
             elif valor > 0:
                 saldo -= valor
                 extrato += f"Saque: R$ {valor:.2f}\n"
                 numero_saques += 1
-            else: print("Sauque - Valor ivalido. Informe valores positivos e maiores de '0'\n")
-        except ValueError: print("\nValor informado é inválido. Digite valores numéricos e inteiros")
+            else: print("\033[0;31mValor ivalido. Informe valores positivos e maiores de '0'\033[m\n")
+        except ValueError: print("\n\033[0;31mValor informado é inválido. Digite valores numéricos e inteiros.\033[m\n")
 
     elif opcao == "E".lower():
-        print('\n'+8*'#'+' EXTRATO '+'#'*8)
+        print('\n'+20*'#'+' EXTRATO '+'#'*20)
+        print(f'Usuario: {user_name} - número de Conta: {account_number} ')
+        print(40*'-')
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print(f"Saldo: R$ {saldo:.2f}")
-        print(18*'#'+'\n')
+        print(40*'#'+'\n')
 
     elif opcao == "Q".lower():
         print(14*'#'+' SAINDO DO SISTEMA '+'#'*14)
-        print('\nA Caixa agradece a sua preferença. Volte sempre')
+        print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
+        print('A Caixa agradece a sua preferença. Volte sempre\n')
         break
 
-    else: print("Opção Inválida. Selecione una opção (letra) valida.\n")
+    else: print("\n\033[0;31mOpção Inválida. Selecione una opção (letra do menu) valida.\033[m\n")
