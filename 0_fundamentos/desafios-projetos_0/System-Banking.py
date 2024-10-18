9529069800edf8d7673a6f3f52dcb6c158a3de4a
Alexis Cervantes <iacervantes@outlook.com>
2024-10-18 19:15:02 -0300

    Atualizando sistema bancario

--- 0_fundamentos/desafios-projetos_0/System-Banking.py
+++ 0_fundamentos/desafios-projetos_0/System-Banking.py
@@ -16,7 +16,7 @@ while True:
     opcao_e = input(external_menu)
 
     if opcao_e == "C".lower():
-        print('\n'+12*'#'+' CRIAR CONTA '+'#'*12)
+        print('\n'+15*'#'+' CRIAR CONTA '+'#'*15)
         user_name = input("Informe seu nome completo: ")
         if all(c.isalpha() or c.isspace() for c in user_name):
             print(f'Seja Bem-vindo {user_name}')
@@ -24,9 +24,9 @@ while True:
         else: print('\033[0;31mDigite valores alfabeticos. Tente de novo\033[m')
 
     elif opcao_e == "X".lower():
-        print(14*'#'+' SAINDO DO SISTEMA '+'#'*14)
+        print('\n'+15*'#'+' SAINDO DO SISTEMA '+'#'*15)
         print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
-        print('A Caixa agradece a sua preferença. Volte sempre\n')
+        print('A Caixa agradece a sua preferença. Volte sempre - SAIDA EXTERNAL\n')
         break
 
     else: print("\n\033[0;31mOpção Inválida. Selecione una opção (letra do menu) valida.(external_menu)\033[m\n")
@@ -35,7 +35,7 @@ while True:
         opcao_i = input(internal_menu)
 
         if opcao_i == "D".lower():
-            print('\n'+12*'#'+' DEPÓSITO '+'#'*12)
+            print('\n'+22*'#'+' DEPÓSITO '+'#'*21)
             print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
             try:
                 valor = float(input("Informe o valor do Depósito: "))
@@ -48,12 +48,13 @@ while True:
             except ValueError: print(f'\033[0;31mValor inválido. Digite valores numéricos e inteiros\033[m\n')
 
         elif opcao_i == "S".lower():
-            print('\n'+12*'#'+' SAQUE '+'#'*12)
+            print('\n'+25*'#'+' SAQUE '+'#'*25)
             print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
             try:
                 print(f'{numero_saques+1} SAQUE')
                 valor = float(input("Informe o valor do Saque: "))
-                print(f'Foram retirados: R$ {valor}\n')
+                print(f'Foram retirados: R$ {valor}')
+                print(53*'_'+'\n')
                 excedeu_saldo = valor > saldo
                 excedeu_limite = valor > limite
                 excedeu_saques = numero_saques >= LIMITE_SAQUES
@@ -68,18 +69,18 @@ while True:
             except ValueError: print("\n\033[0;31mValor informado é inválido. Digite valores numéricos e inteiros.\033[m\n")
 
         elif opcao_i == "E".lower():
-            print('\n'+24*'#'+' EXTRATO '+'#'*24)
+            print('\n'+23*'#'+' EXTRATO '+'#'*22)
             print(f'Usuario: {user_name} - número de Conta: {account_number} ')
-            print(53*'-')
+            print(30*'-')
             print("Não foram realizadas movimentações." if not extrato else extrato)
-            print(53*'_')
+            print(30*'_')
             print(f"Saldo: R$ {saldo:.2f}")
             print(53*'#'+'\n')
 
         elif opcao_i == "Q".lower():
-            print(14*'#'+' SAINDO DO SISTEMA '+'#'*14)
+            print('\n'+26*'#'+' SAINDO DO SISTEMA '+'#'*26)
             print(f'Usuario: {user_name} - Número de Conta: {account_number} ')
-            print('A Caixa agradece a sua preferença. Volte sempre\n')
+            print('A Caixa agradece a sua preferença. Volte sempre - SAIDA INTERNAL\n')
             break
 
         else: print("\n\033[0;31mOpção Inválida. Selecione una opção (letra do menu) valida.(internal_menu)\033[m\n")
